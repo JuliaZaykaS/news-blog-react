37e5eb46a83cdd8af4079eae7b143ea4c5648e09
JuliaZaykaS <sys55@mail.ru>
2022-01-27 14:32:38 +0300

    start

---
+++ src/redux/store.js
@@ -0,0 +1,30 @@
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+  } from 'redux-persist';
+  import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+
+  const middleware = [
+    ...getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  ];
+
+  const store = configureStore({
+    reducer: {
+    //   auth: authReducer,
+    //   news: newsReducer,
+    },
+    middleware,
+    devTools: process.env.NODE_ENV === 'development',
+  });
+
+  export { store};
